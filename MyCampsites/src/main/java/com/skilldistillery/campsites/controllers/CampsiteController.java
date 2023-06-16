4f69b00843d55e3d03a52a6f4d6593c74509d1c1
Jacob <jwv928@gmail.com>
2023-06-16 16:48:24 -0400

    added anotation to Controllers

--- MyCampsites/src/main/java/com/skilldistillery/campsites/controllers/CampsiteController.java
+++ MyCampsites/src/main/java/com/skilldistillery/campsites/controllers/CampsiteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.skilldistillery.campsites.entities.Campsite;
 import com.skilldistillery.campsites.entities.Location;
 import com.skilldistillery.campsites.services.CampsiteService;
 
+@CrossOrigin({"*", "http://localhost/"})
 @RestController
 @RequestMapping("api")
 public class CampsiteController {
--- MyCampsites/src/main/java/com/skilldistillery/campsites/controllers/CommentController.java
+++ MyCampsites/src/main/java/com/skilldistillery/campsites/controllers/CommentController.java
@@ -3,6 +3,7 @@ package com.skilldistillery.campsites.controllers;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.skilldistillery.campsites.entities.Comment;
 import com.skilldistillery.campsites.services.CommentService;
 
+@CrossOrigin({"*", "http://localhost/"})
 @RestController
 @RequestMapping("api")
 public class CommentController {
